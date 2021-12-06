f4b424e120bb4dc12fbd38e4a7cb1dfda91293da
ahkui <ahkui@outlook.com>
2021-12-07 01:12:52 +0800

    3.1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,9 @@
 <template>
   <div>
-    <input type="text" v-model="todoInput" />
-    <button @click="addTodo">Add</button>
-
-    <ul>
-      <li v-for="item in todos" :key="item.id">{{ item.text }}</li>
-    </ul>
+    <todo-list></todo-list>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
-
-const nextId = ref(4);
-const todos = ref([
-  { id: 1, text: "Learn Vue 3" },
-  { id: 2, text: "Create Todo App" },
-  { id: 3, text: "Feedback" },
-]);
-
-const todoInput = ref("");
-function addTodo() {
-  todos.value.push({ id: nextId.value, text: todoInput.value });
-  nextId.value++;
-  todoInput.value = "";
-}
+import TodoList from './components/TodoList.vue';
 </script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,26 @@
+<template>
+  <input type="text" v-model="todoInput" />
+  <button @click="addTodo">Add</button>
+
+  <ul>
+    <li v-for="item in todos" :key="item.id">{{ item.text }}</li>
+  </ul>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const nextId = ref(4);
+const todos = ref([
+  { id: 1, text: "Learn Vue 3" },
+  { id: 2, text: "Create Todo App" },
+  { id: 3, text: "Feedback" },
+]);
+
+const todoInput = ref("");
+function addTodo() {
+  todos.value.push({ id: nextId.value, text: todoInput.value });
+  nextId.value++;
+  todoInput.value = "";
+}
+</script>
