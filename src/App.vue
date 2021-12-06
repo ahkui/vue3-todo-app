f937eafc047488e1791cb30fa5baf767ef657d05
ahkui <ahkui@outlook.com>
2021-12-07 00:34:34 +0800

    2.1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <h1>Hello {{ world }}</h1>
-  <input type="text" v-model="world" />
+  <ul>
+    <li>{{ todos[0].text }}</li>
+    <li>{{ todos[1].text }}</li>
+    <li>{{ todos[2].text }}</li>
+  </ul>
 </template>
 
 <script setup>
 import { ref } from "vue";
 
-const world = ref("world");
+const todos = ref([
+  { id: 1, text: "Learn Vue 3" },
+  { id: 2, text: "Create Todo App" },
+  { id: 3, text: "Feedback" },
+]);
 </script>
