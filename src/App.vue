c7b0d374cdca9af64ce9a8f63593426f6104e396
ahkui <ahkui@outlook.com>
2021-12-07 00:46:16 +0800

    2.3

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
-  <ul>
-    <li v-for="item in todos" :key="item.id">{{ item.text }}</li>
-  </ul>
+  <div>
+    <input type="text" v-model="todoInput" />
+    <button @click="addTodo">Add</button>
+
+    <ul>
+      <li v-for="item in todos" :key="item.id">{{ item.text }}</li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 
+const nextId = ref(4);
 const todos = ref([
   { id: 1, text: "Learn Vue 3" },
   { id: 2, text: "Create Todo App" },
   { id: 3, text: "Feedback" },
 ]);
+
+const todoInput = ref("");
+function addTodo() {
+  todos.value.push({ id: nextId.value, text: todoInput.value });
+  nextId.value++;
+  todoInput.value = "";
+}
 </script>
